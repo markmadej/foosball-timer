2c4dd0d65aa9ceec5baf6e87558b8317ff662491
markmadej <mark.madej@gmail.com>
2017-03-21 12:06:52 -0600

    Switching position of 5 bar and 3 bar timers

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <RodTimeClock title='2/3 Bars' maxSeconds='15'/>
         <RodTimeClock title='5 Bar' maxSeconds='10'/>
+        <RodTimeClock title='2/3 Bars' maxSeconds='15'/>
       </div>
     );
   }
